592c181a1e567c41c4654d1396ee4e7370213ff3
jay <jayfsalvacion@iskolarngbayan.pup.edu.ph>
2023-06-19 10:43:15 +0800

    Authentication (Initial)

--- app/src/main/java/com/qol/buddyb/Register.kt
+++ app/src/main/java/com/qol/buddyb/Register.kt
@@ -1,6 +1,9 @@
 package com.qol.buddyb
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
@@ -10,13 +13,51 @@ import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import com.google.android.gms.tasks.OnCompleteListener
+import com.google.firebase.auth.AuthResult
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
 import androidx.compose.ui.tooling.preview.Preview
+import com.qol.buddyb.databinding.ActivityRegisterBinding
 import com.qol.buddyb.ui.theme.BuddyBTheme
 
 class Register : AppCompatActivity() {
 
+    private lateinit var binding: ActivityRegisterBinding
+    private lateinit var auth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_register)
+        binding = ActivityRegisterBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        auth = FirebaseAuth.getInstance()
+
+        binding.btnRegister.setOnClickListener{
+            val email = binding.signupEmail.text.toString()
+            val password = binding.signupPassword.text.toString()
+            val confirmPassword = binding.signupConfirm.text.toString()
+
+            if (email.isNotEmpty() && password.isNotEmpty() && confirmPassword.isNotEmpty()){
+                if (password == confirmPassword){
+                    auth.createUserWithEmailAndPassword(email, password).addOnCompleteListener{
+                        if (it.isSuccessful){
+                            val intent = Intent(this, Login::class.java)
+                            startActivity(intent)
+                        } else {
+                            Toast.makeText(this, it.exception.toString(), Toast.LENGTH_SHORT).show()
+                        }
+                    }
+                } else {
+                    Toast.makeText(this, "Passwords doesn't match", Toast.LENGTH_SHORT).show()
+                }
+            } else {
+                Toast.makeText(this, "All fields cannot be empty.", Toast.LENGTH_SHORT).show()
+            }
+        }
+        binding.loginredirect.setOnClickListener{
+            val loginIntent = Intent(this, Login::class.java)
+            startActivity(loginIntent)
+        }
     }
 }
