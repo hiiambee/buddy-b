531abe2e78cbb0143401f45eff9a54d6c02febdd
jay <jayfsalvacion@iskolarngbayan.pup.edu.ph>
2023-06-19 10:43:15 +0800

    Authentication (Initial) Splash Screen (To login page)

--- app/src/main/java/com/qol/buddyb/Login.kt
+++ app/src/main/java/com/qol/buddyb/Login.kt
@@ -1,7 +1,13 @@
 package com.qol.buddyb
 
+import android.content.Intent
 import android.os.Bundle
+import android.graphics.drawable.ColorDrawable
 import android.os.PersistableBundle
+import android.widget.Button
+import android.widget.EditText
+import androidx.appcompat.app.AlertDialog
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
@@ -11,13 +17,46 @@ import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import com.google.android.gms.tasks.OnCompleteListener
+import com.google.firebase.auth.AuthResult
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
 import androidx.compose.ui.tooling.preview.Preview
+import com.qol.buddyb.databinding.ActivityLoginBinding
 import com.qol.buddyb.ui.theme.BuddyBTheme
 
 class Login : AppCompatActivity() {
 
+    private lateinit var binding: ActivityLoginBinding
+    private lateinit var auth: FirebaseAuth
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        auth = FirebaseAuth.getInstance()
+
+        binding.btnLogin.setOnClickListener {
+            val email = binding.loginEmail.text.toString()
+            val password = binding.loginPassword.text.toString()
+
+            if (email.isNotEmpty() && password.isNotEmpty()){
+
+                auth.signInWithEmailAndPassword(email, password).addOnCompleteListener{
+                    if (it.isSuccessful){
+                        val intent = Intent(this, MainActivity::class.java)
+                        startActivity(intent)
+                    } else {
+                        Toast.makeText(this, it.exception.toString(), Toast.LENGTH_SHORT).show()
+                    }
+                }
+            } else {
+                Toast.makeText(this, "Please input your login credentials.", Toast.LENGTH_SHORT).show()
+            }
+        }
+        binding.signupredirect.setOnClickListener{
+            val signupIntent = Intent(this, Register::class.java)
+            startActivity(signupIntent)
+        }
     }
 }
