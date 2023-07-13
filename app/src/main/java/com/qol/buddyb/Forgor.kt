988ae308f8b3259f117e03692502c7a9792b5ce8
jay <jayfsalvacion@iskolarngbayan.pup.edu.ph>
2023-07-13 13:19:19 +0800

    Forgot Password 2

--- app/src/main/java/com/qol/buddyb/Forgor.kt
+++ app/src/main/java/com/qol/buddyb/Forgor.kt
@@ -1,6 +1,7 @@
 package com.qol.buddyb
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
@@ -10,16 +11,36 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.google.firebase.auth.FirebaseAuth
 import com.qol.buddyb.databinding.ActivityForgorBinding
 import com.qol.buddyb.ui.theme.BuddyBTheme
 
 class Forgor : ComponentActivity() {
 
     private lateinit var binding: ActivityForgorBinding
+    private lateinit var auth: FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityForgorBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        auth = FirebaseAuth.getInstance()
+
+        binding.btnForgor.setOnClickListener {
+            val email = binding.forgorEmail.text.toString()
+
+            if (email.isNotEmpty()) {
+                auth.sendPasswordResetEmail(email)
+                    .addOnSuccessListener {
+                        Toast.makeText(this, "Please check your mail", Toast.LENGTH_SHORT).show()
+                    }
+                    .addOnFailureListener {
+                        Toast.makeText(this, it.toString(), Toast.LENGTH_SHORT).show()
+                    }
+            } else {
+                Toast.makeText(this, "Please input your login credentials.", Toast.LENGTH_SHORT).show()
             }
         }
+    }
+}
