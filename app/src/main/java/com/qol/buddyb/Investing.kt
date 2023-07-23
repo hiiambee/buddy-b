f2bc582adc57a9b7e2d83619b77278f7adfe9517
Jhonas <jhonasabaja@gmail.com>
2023-07-23 18:03:36 +0800

    intro 1/2/3

--- app/src/main/java/com/qol/buddyb/Investing.kt
+++ app/src/main/java/com/qol/buddyb/Investing.kt
@@ -34,7 +34,7 @@ class Investing : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.balance_4, container, false)
+        return inflater.inflate(R.layout.fragment_investing, container, false)
     }
 
     companion object {
