efb6eac42735c305fa09295f30981fb013836ca4
jay <jayfsalvacion@iskolarngbayan.pup.edu.ph>
2023-07-28 22:00:53 +0800

    Bill Check and Delete

--- app/src/main/java/com/qol/buddyb/BillAdapter.kt
+++ app/src/main/java/com/qol/buddyb/BillAdapter.kt
@@ -12,6 +12,8 @@ import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
 import java.time.Instant
+import kotlin.collections.hashMapOf
+import kotlin.collections.hashMapOf as hashMapOf1
 
 class BillAdapter(private val billList: ArrayList<Bills>) : RecyclerView.Adapter<BillAdapter.MyViewHolder>() {
 
@@ -32,6 +34,69 @@ class BillAdapter(private val billList: ArrayList<Bills>) : RecyclerView.Adapter
         holder.deleteButton.setOnClickListener {
             deleteBill(position)
         }
+
+        holder.checkbutton.setOnClickListener{
+            checkbill(position)
+        }
+    }
+
+    private fun checkbill(position: Int) {
+        db = FirebaseFirestore.getInstance()
+        auth = FirebaseAuth.getInstance()
+        val uid = FirebaseAuth.getInstance().currentUser!!.uid
+
+        // Get the bill document ID from the Firestore based on its position in the list
+        val billId = billList[position].documentId
+
+        db.collection("Database").document(uid)
+            .collection("bill").document(billId!!)
+            .get()
+            .addOnSuccessListener {
+                if (it != null) {
+                    val title = it.data?.get("title")?.toString()
+                    val amount = it.data?.get("amount")
+                    val due = it.data?.get("due")
+
+                    val billmap = hashMapOf(
+                        "title" to title,
+                        "amount" to amount,
+                        "date" to due
+                    )
+
+                    db.collection("Database").document(uid)
+                        .collection("expense")
+                        .document()
+                        .set(billmap)
+                        .addOnSuccessListener {
+
+                            // Remove the bill from Firestore
+                            db.collection("Database").document(uid)
+                                .collection("bill").document(billId!!)
+                                .delete()
+                                .addOnSuccessListener {
+                                    // Remove the bill from the list and update the RecyclerView
+                                    billList.removeAt(position)
+                                    notifyItemRemoved(position)
+                                }
+                                .addOnFailureListener { e ->
+                                    // Handle any errors that occur during deletion
+                                    Log.e("BillAdapter", "Error deleting bill: ${e.message}")
+                                }
+
+                        }
+                        .addOnFailureListener { e ->
+                            // Handle any errors that occur during deletion
+                            Log.e("BillAdapter", "Error deleting bill: ${e.message}")
+                        }
+
+                }
+
+
+            }
+            .addOnFailureListener { e ->
+                // Handle any errors that occur during deletion
+                Log.e("BillAdapter", "Error deleting bill: ${e.message}")
+            }
     }
 
     override fun getItemCount(): Int {
@@ -66,5 +131,6 @@ class BillAdapter(private val billList: ArrayList<Bills>) : RecyclerView.Adapter
         val amount: TextView = itemView.findViewById(R.id.billprice)
         val due: TextView = itemView.findViewById(R.id.billdate)
         val deleteButton: ImageView = itemView.findViewById(R.id.b_delbutton)
+        val checkbutton: ImageView = itemView.findViewById(R.id.b_checkbutton)
     }
 }
