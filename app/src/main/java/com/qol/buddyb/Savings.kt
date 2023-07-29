9e0c567a14c47d0793e92f65ac6cfdf69924a10c
jay <jayfsalvacion@iskolarngbayan.pup.edu.ph>
2023-07-29 20:19:42 +0800

    Savings Complete

--- app/src/main/java/com/qol/buddyb/Savings.kt
+++ app/src/main/java/com/qol/buddyb/Savings.kt
@@ -58,6 +58,7 @@ class Savings : Fragment() {
 
         fetchsavdata()
         fetchexpdata()
+        fetchmoneydata()
 
         val addbutton = rootView.findViewById<ImageView>(R.id.addsavbutton)
         addbutton.setOnClickListener{
@@ -137,6 +138,69 @@ class Savings : Fragment() {
 
 
             })
+    }
+
+    private fun fetchmoneydata() {
+        db = FirebaseFirestore.getInstance()
+        auth = FirebaseAuth.getInstance()
+        val uid = FirebaseAuth.getInstance().currentUser!!.uid
+        val mainCollectionRef = db.collection("Database")
+        val subCollectionRef = mainCollectionRef.document(uid).collection("expense")
+        val subCollectionRef2 = mainCollectionRef.document(uid).collection("saving")
+        var totalbal = 0
+
+        subCollectionRef2.get()
+            .addOnSuccessListener { documents ->
+                if (!documents.isEmpty) {
+                    var sum2 = 0
+
+                    for (document in documents) {
+                        val savingData = document.toObject(Saving::class.java)
+                        sum2 += savingData.amount?: 0
+                    }
+                    totalbal += sum2
+                    val amountRedTextView = view?.findViewById<TextView>(R.id.inc)
+                    amountRedTextView?.text = "PHP $sum2"
+
+                } else {
+                    val amountRedTextView = view?.findViewById<TextView>(R.id.inc)
+                    amountRedTextView?.text = "PHP 0.00"
+                }
+
+                subCollectionRef.get()
+                    .addOnSuccessListener { documents ->
+                        if (!documents.isEmpty) {
+                            var sum = 0
+
+                            for (document in documents) {
+                                val expenseData = document.toObject(Expense::class.java)
+                                sum += expenseData.amount?: 0
+                            }
+                            totalbal -= sum
+                            val amountRedTextView = view?.findViewById<TextView>(R.id.exp)
+                            amountRedTextView?.text = "PHP $sum"
+                            val amountbalTextView = view?.findViewById<TextView>(R.id.totalbal)
+                            amountbalTextView?.text = "PHP $totalbal"
+
+                        } else {
+                            val amountRedTextView = view?.findViewById<TextView>(R.id.exp)
+                            amountRedTextView?.text = "PHP 0.00"
+                        }
+                    }
+                    .addOnFailureListener { exception ->
+                        val amountRedTextView = view?.findViewById<TextView>(R.id.exp)
+                        amountRedTextView?.text = "OFFLINE"
+                    }
+
+
+            }
+            .addOnFailureListener { exception ->
+                val amountRedTextView = view?.findViewById<TextView>(R.id.inc)
+                amountRedTextView?.text = "OFFLINE"
+                val amountExpTextView = view?.findViewById<TextView>(R.id.exp)
+                amountExpTextView?.text = "OFFLINE"
+            }
+
     }
 
 
