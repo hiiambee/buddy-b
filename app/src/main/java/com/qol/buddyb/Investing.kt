8e98b84269a3438e634623638ba07a8dd5040dd5
jay <jayfsalvacion@iskolarngbayan.pup.edu.ph>
2023-07-30 12:13:06 +0800

    Full App Completion and Release

--- app/src/main/java/com/qol/buddyb/Investing.kt
+++ app/src/main/java/com/qol/buddyb/Investing.kt
@@ -24,8 +24,188 @@ class Investing : Fragment() {
 
         fetchappledata(rootView)
         fetchgoogledata(rootView)
+        fetchmsftdata(rootView)
+        fetchamzndata(rootView)
+        fetchrblxdata(rootView)
 
         return rootView
+    }
+
+    private fun fetchrblxdata(rootView: View) {
+        val baseUrl = "http://api.marketstack.com/v1/"
+        val accessKey = "4794206a750f0b853e937da6726c67c3"
+        val retrofit = Retrofit.Builder()
+            .baseUrl(baseUrl)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+        val marketStackService = retrofit.create(MarketStackApiService::class.java)
+        val symbols = "RBLX"
+
+        val call: Call<MarketStackResponse> = marketStackService.getMarketData(accessKey, symbols)
+        call.enqueue(object : Callback<MarketStackResponse> {
+            override fun onResponse(call: Call<MarketStackResponse>, response: Response<MarketStackResponse>) {
+                if (response.isSuccessful) {
+                    val marketDataResponse = response.body()
+                    if (marketDataResponse != null) {
+                        val marketDataList = marketDataResponse.data
+                        if (marketDataList.isNotEmpty()) {
+                            // Use the marketDataList[0] (first item) here to access the symbol, exchange, open, close, high, low, and volume.
+                            val marketData = marketDataList[0]
+                            val symbol = marketData.symbol
+                            val exchange = marketData.exchange
+                            val open = marketData.open
+                            val close = marketData.close
+                            val high = marketData.high
+                            val low = marketData.low
+                            val volume = marketData.volume
+
+                            // Update the TextViews
+                            val rblx = rootView.findViewById<TextView>(R.id.symbol5)
+                            val rblxex = rootView.findViewById<TextView>(R.id.exchange5)
+                            val rblxop = rootView.findViewById<TextView>(R.id.open5)
+                            val rblxcl = rootView.findViewById<TextView>(R.id.close5)
+                            val rblxhi = rootView.findViewById<TextView>(R.id.high5)
+                            val rblxlo = rootView.findViewById<TextView>(R.id.low5)
+                            val rblxvo = rootView.findViewById<TextView>(R.id.volume5)
+
+                            rblx.text = "Roblox Corp. - $symbol"
+                            rblxex.text = "$exchange Exchange"
+                            rblxop.text = "Open: $open"
+                            rblxcl.text = "Close: $close"
+                            rblxhi.text = "High: $high"
+                            rblxlo.text = "Low: $low"
+                            rblxvo.text = "Volume: $volume"
+                        }
+                    }
+                } else {
+                    // Log the API error
+                    Log.e("InvestingFragment", "API call failed: ${response.code()} - ${response.message()}")
+                }
+            }
+
+            override fun onFailure(call: Call<MarketStackResponse>, t: Throwable) {
+                Log.e("InvestingFragment", "API call failed: ${t.message}")
+            }
+        })
+
+    }
+
+    private fun fetchamzndata(rootView: View) {
+        val baseUrl = "http://api.marketstack.com/v1/"
+        val accessKey = "4794206a750f0b853e937da6726c67c3"
+        val retrofit = Retrofit.Builder()
+            .baseUrl(baseUrl)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+        val marketStackService = retrofit.create(MarketStackApiService::class.java)
+        val symbols = "AMZN"
+
+        val call: Call<MarketStackResponse> = marketStackService.getMarketData(accessKey, symbols)
+        call.enqueue(object : Callback<MarketStackResponse> {
+            override fun onResponse(call: Call<MarketStackResponse>, response: Response<MarketStackResponse>) {
+                if (response.isSuccessful) {
+                    val marketDataResponse = response.body()
+                    if (marketDataResponse != null) {
+                        val marketDataList = marketDataResponse.data
+                        if (marketDataList.isNotEmpty()) {
+                            // Use the marketDataList[0] (first item) here to access the symbol, exchange, open, close, high, low, and volume.
+                            val marketData = marketDataList[0]
+                            val symbol = marketData.symbol
+                            val exchange = marketData.exchange
+                            val open = marketData.open
+                            val close = marketData.close
+                            val high = marketData.high
+                            val low = marketData.low
+                            val volume = marketData.volume
+
+                            // Update the TextViews
+                            val amzn = rootView.findViewById<TextView>(R.id.symbol4)
+                            val amznex = rootView.findViewById<TextView>(R.id.exchange4)
+                            val amznop = rootView.findViewById<TextView>(R.id.open4)
+                            val amzncl = rootView.findViewById<TextView>(R.id.close4)
+                            val amznhi = rootView.findViewById<TextView>(R.id.high4)
+                            val amznlo = rootView.findViewById<TextView>(R.id.low4)
+                            val amznvo = rootView.findViewById<TextView>(R.id.volume4)
+
+                            amzn.text = "Amazon.com - $symbol"
+                            amznex.text = "$exchange Exchange"
+                            amznop.text = "Open: $open"
+                            amzncl.text = "Close: $close"
+                            amznhi.text = "High: $high"
+                            amznlo.text = "Low: $low"
+                            amznvo.text = "Volume: $volume"
+                        }
+                    }
+                } else {
+                    // Log the API error
+                    Log.e("InvestingFragment", "API call failed: ${response.code()} - ${response.message()}")
+                }
+            }
+
+            override fun onFailure(call: Call<MarketStackResponse>, t: Throwable) {
+                Log.e("InvestingFragment", "API call failed: ${t.message}")
+            }
+        })
+
+    }
+
+    private fun fetchmsftdata(rootView: View) {
+        val baseUrl = "http://api.marketstack.com/v1/"
+        val accessKey = "4794206a750f0b853e937da6726c67c3"
+        val retrofit = Retrofit.Builder()
+            .baseUrl(baseUrl)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+        val marketStackService = retrofit.create(MarketStackApiService::class.java)
+        val symbols = "MSFT"
+
+        val call: Call<MarketStackResponse> = marketStackService.getMarketData(accessKey, symbols)
+        call.enqueue(object : Callback<MarketStackResponse> {
+            override fun onResponse(call: Call<MarketStackResponse>, response: Response<MarketStackResponse>) {
+                if (response.isSuccessful) {
+                    val marketDataResponse = response.body()
+                    if (marketDataResponse != null) {
+                        val marketDataList = marketDataResponse.data
+                        if (marketDataList.isNotEmpty()) {
+                            // Use the marketDataList[0] (first item) here to access the symbol, exchange, open, close, high, low, and volume.
+                            val marketData = marketDataList[0]
+                            val symbol = marketData.symbol
+                            val exchange = marketData.exchange
+                            val open = marketData.open
+                            val close = marketData.close
+                            val high = marketData.high
+                            val low = marketData.low
+                            val volume = marketData.volume
+
+                            // Update the TextViews
+                            val msft = rootView.findViewById<TextView>(R.id.symbol3)
+                            val msftex = rootView.findViewById<TextView>(R.id.exchange3)
+                            val msftop = rootView.findViewById<TextView>(R.id.open3)
+                            val msftcl = rootView.findViewById<TextView>(R.id.close3)
+                            val msfthi = rootView.findViewById<TextView>(R.id.high3)
+                            val msftlo = rootView.findViewById<TextView>(R.id.low3)
+                            val msftvo = rootView.findViewById<TextView>(R.id.volume3)
+
+                            msft.text = "Microsoft Corp. - $symbol"
+                            msftex.text = "$exchange Exchange"
+                            msftop.text = "Open: $open"
+                            msftcl.text = "Close: $close"
+                            msfthi.text = "High: $high"
+                            msftlo.text = "Low: $low"
+                            msftvo.text = "Volume: $volume"
+                        }
+                    }
+                } else {
+                    // Log the API error
+                    Log.e("InvestingFragment", "API call failed: ${response.code()} - ${response.message()}")
+                }
+            }
+
+            override fun onFailure(call: Call<MarketStackResponse>, t: Throwable) {
+                Log.e("InvestingFragment", "API call failed: ${t.message}")
+            }
+        })
+
     }
 
     private fun fetchgoogledata(rootView: View) {
